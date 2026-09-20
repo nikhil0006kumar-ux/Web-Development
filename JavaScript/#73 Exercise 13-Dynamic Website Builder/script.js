f4b86d77f7a658ab6cd7351051d4007243ef0491
Nikhil Kumar <nikhil0006kumar@gmail.com>
2026-09-20 12:25:56 +0530

    Add files via upload

---
+++ JavaScript/#73 Exercise 13-Dynamic Website Builder/script.js
@@ -0,0 +1,5 @@
+function createCard(tittle, cName, views, monthsOld, duration,
+    thumbnail){
+    // Finish this function
+}
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22, https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLB6VzxANxt3dHm91HW9lLfDlxvmuA")
